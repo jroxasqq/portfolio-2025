efd45a7883e6da670e0133b7baf32e907ba590d0
jroxasqq <yourName@student.unsw.edu.au>
2025-09-18 23:22:19 +1000

    Fixed issues with project item background coloring

--- src/components/ProjectsContentItemEnd.tsx
+++ src/components/ProjectsContentItemEnd.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ProjectsContentItemEnd = () => {
   return (
-    <div className="bg-yellow-300 text-3xl font-bold items-center justify-center flex">
+    <div className="bg-yellow-300 bg-blue-300 bg-red-300 bg-green-300 text-3xl font-bold items-center justify-center flex">
       that&apos;s all, more soon!
     </div>
   );
