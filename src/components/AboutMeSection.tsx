6f0aa23909bce33f517a64d02d4a4aad57f88bef
jroxasqq <yourName@student.unsw.edu.au>
2025-12-10 13:25:01 +1100

    fixed text wrapping issue for "about me" section

--- src/components/AboutMeSection.tsx
+++ src/components/AboutMeSection.tsx
@@ -6,34 +6,42 @@ const AboutMeSection = () => {
     <div className="relative">
       <div className="h-screen w-screen justify-evenly content-center flex">
         <div className="content-center">
-          <Image className="h-100 w-100" src={geometricImage} alt="image of geometric object" />
+          <Image
+            className="h-100 w-100 invisible lg:visible"
+            src={geometricImage}
+            alt="image of geometric object"
+          />
         </div>
         <div className="content-center">
-          <p className="text-[6.5em] font-extrabold text-[#5445F6]">ABOUT ME</p>
-          <p className="text-5xl text-[#5445F6]">
+          <p className="font-extrabold text-[6.5em] text-[#5445F6] text-nowrap">ABOUT ME</p>
+          <p className="text-5xl text-[#5445F6] text-nowrap">
             <span className="text-[#5445F6]/60">%#:%^/ /</span> JUSTIN ROXAS
           </p>
-          <p className="text-5xl text-[#5445F6]">
+          <p className="text-5xl text-[#5445F6] text-nowrap">
             WEB DEVELOPER <span className="text-[#5445F6]/60">/ /&#%</span>
           </p>
-          <p className="text-5xl text-[#5445F6]">
+          <p className="text-5xl text-[#5445F6] text-nowrap">
             <span className="text-[#5445F6]/60">#&^!%*^#&*~^&!/ /</span> WEB
           </p>
-          <p className="text-5xl text-[#5445F6]">
+          <p className="text-5xl text-[#5445F6] text-nowrap">
             DESIGNER <span className="text-[#5445F6]/60">/ /^#@$^@#*</span>
           </p>
-          <p className="text-5xl text-[#5445F6]">
+          <p className="text-5xl text-[#5445F6] text-nowrap">
             <span className="text-[#5445F6]/60">$!%&!^@#/ /</span> ASIAN FILM
           </p>
-          <p className="text-5xl text-[#5445F6]">
+          <p className="text-5xl text-[#5445F6] text-nowrap">
             LOVER <span className="text-[#5445F6]/60">/ /@?&^#/ / </span>IDEAS
           </p>
-          <p className="text-5xl text-[#5445F6]">
+          <p className="text-5xl text-[#5445F6] text-nowrap">
             INTO REALITY <span className="text-[#5445F6]/60">/ /@$&^/ / </span>
           </p>
         </div>
         <div className="content-center">
-          <Image className="h-100 w-100" src={geometricImage} alt="image of geometric object" />
+          <Image
+            className="h-100 w-100 invisible lg:visible"
+            src={geometricImage}
+            alt="image of geometric object"
+          />
         </div>
       </div>
       <div className="text-xl font-bold text-black/60 absolute bottom-5 left-17">
